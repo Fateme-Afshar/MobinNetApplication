2a5cb816f3a6abd08639ae9b24ca16377719be3d
FatemeAfshar20 <uttab4760@gmail.com>
2021-09-15 23:50:08 -0700

    Creating fragment_login.xml.

--- app/src/main/java/com/example/mobinnetapplication/view/activity/BaseActivity.kt
+++ app/src/main/java/com/example/mobinnetapplication/view/activity/BaseActivity.kt
@@ -1,12 +1,24 @@
 package com.example.mobinnetapplication.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.mobinnetapplication.R
+import com.example.mobinnetapplication.databinding.ActivityBaseBinding
+import com.example.mobinnetapplication.view.fragment.LoginFragment
 
 class BaseActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBaseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_base)
+
+        val fragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (fragment == null)
+            supportFragmentManager.beginTransaction().
+            add(R.id.fragment_container, LoginFragment())
+                .commit()
     }
 }
---
+++ app/src/main/java/com/example/mobinnetapplication/view/fragment/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.example.mobinnetapplication.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.mobinnetapplication.R
+import com.example.mobinnetapplication.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+}
